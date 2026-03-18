477f2aec566dc1cd1e91df658bba7e7dfe3239b7
Mafaro <mafaro2105@gmail.com>
2026-03-18 12:54:52 +0200

    forgot to save file

--- app/auth/invitation/page.tsx
+++ app/auth/invitation/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = "force-dynamic";
 import { useSearchParams, useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { useEffect } from 'react'
